a58440b6f62748819c039af62cf92f4854de5830
landkommunenhippie <hahn.tobias66@gmail.com>
2016-04-07 14:31:16 +0200

    added arguments for execution

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,25 @@
 ///<reference path="scraper/CplScraper.ts"/> 
 
-var scraper = new Scraper.CplHeloiseScraper();
-//scraper.scrapeIndex();
-scraper.scrapeDetails();
+var appArgLength: number = process.argv.length;
+var execution: boolean = true;
+if(!(appArgLength > 2)){
+	console.error("Arguments are missing.");
+}else{
+	
+	//switch scraper
+	if(process.argv[2] == "cpl"){
+		var scraper = new Scraper.CplHeloiseScraper();
+	}else{
+		console.log("No Scraper for arg %s found.", process.argv[2]);
+		execution = false;
+		}
+		if(execution){
+			// start scraping
+			if(process.argv[3] == "details"){
+				scraper.scrapeDetails();	
+			}else{
+				scraper.scrapeIndex();
+			}
+		}
+
+}
