a7c8618e13839e9e52685444fe98bcc883de0cdf
landkommunenhippie <hahn.tobias66@gmail.com>
2016-06-22 21:13:30 +0200

    finished crawling of csv-file, just minor-properties are parsed

--- src/app/crawler/ParisiensCrawler.ts
+++ src/app/crawler/ParisiensCrawler.ts
@@ -1,11 +1,13 @@
 ///<reference path="HeloiseCrawler.ts"/> 
+///<reference path="../system/system.ts"/> 
 ///<reference path="html/CplHeloiseParser.ts"/> 
+///<reference path="http/ParisiensHttpRequester.ts"/> 
 module Crawler{
 
 export class ParisiensCrawler extends HeloiseCrawler{
 	private profs: DTO.ProfDto[] = [];
 	private Converter = require('csvtojson').Converter;
-	private csvConverter = new this.Converter({});
+	private csvConverter = new this.Converter({headers:['id','fullName','mediante','status','origine','diocèse','university','title','religion','professional','author']});
 
 
 	constructor(){
@@ -24,7 +26,18 @@ export class ParisiensCrawler extends HeloiseCrawler{
 	}
 
 	private parseCsv = (err, jsonResult:any) => {
-		console.log(jsonResult);
+		let requester : Requester.HttpRequester = new Requester.ParisiensHttpRequester();
+		for(var rowIndex in jsonResult){
+			let rowObject = jsonResult[rowIndex];
+			var prof: DTO.ProfDto = new DTO.ProfDto();
+			var title :string  = rowObject.title;
+			console.log(title.split('\n').join("").replace(/\s+/g, ' ').replace(".",'').trim());
+			prof.title = title.split('\n').join("").trim() ;
+			prof.name = rowObject.fullName;
+			prof.id = rowObject.id;
+			prof.title = rowObject.title;
+			requester.postToEs(prof);
+		}
 	}
 }
 
