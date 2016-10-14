661664e7d6c113dd5c49e3a533bb2b428e672f64
landkommunenhippie <hahn.tobias66@gmail.com>
2016-10-14 16:41:48 +0200

    created iteration for whole index

--- src/app/crawler/RagCrawler.ts
+++ src/app/crawler/RagCrawler.ts
@@ -4,17 +4,22 @@
 module Crawler{
     export class RagCrawler extends HeloiseCrawler{
         private profs: DTO.ProfDto[] = [];
-
+        private requestIndexItemsLimit:number = 10; 
         constructor(){
             super();
-            this.requester = new Requester.RagHttpRequester(50);
+            this.requester = new Requester.RagHttpRequester(this.requestIndexItemsLimit);
             this.requester.registerObserver(this);
             //this.parser = new Parser.CplHeloiseParser();
         }
 
         public update(dto: DTO.JsonDto):void{
             if(dto.type == DTO.PageType.INDEX){
-              console.log(dto.obj);
+              console.log(typeof dto.obj);
+              let numberOfResults:number = dto.obj["iTotalDisplayRecords"];
+              let numberOfRequests = Math.floor(numberOfResults / this.requestIndexItemsLimit);
+              console.log(numberOfResults + " : " + numberOfRequests);
+              this.crawlFurtherIndexPages(numberOfRequests);
+
             }else if(dto.type == DTO.PageType.SINGLE_INDEX){
               console.log(dto.obj);
             }else if(dto.type == DTO.PageType.DETAIL){
@@ -25,6 +30,14 @@ module Crawler{
         public crawlIndex(){
 		    this.requester.requestIndexPage();
 	    }
+
+        private crawlFurtherIndexPages(numberOfRequests: number){
+            if(numberOfRequests > 0 ){
+                //this.requester.requestIndexPage(index);
+                setTimeout(() => {this.crawlFurtherIndexPages(--numberOfRequests);},3000);
+		        console.info("processe Index Page: " + numberOfRequests);
+            }
+        }
 
     } 
 }
