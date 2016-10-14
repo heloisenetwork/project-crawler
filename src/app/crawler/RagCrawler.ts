7b3db802348461ab7e588c36e3af86db617b8627
landkommunenhippie <hahn.tobias66@gmail.com>
2016-10-14 12:20:08 +0200

    added new crawler

---
+++ src/app/crawler/RagCrawler.ts
@@ -0,0 +1,8 @@
+///<reference path="HeloiseCrawler.ts"/> 
+///<reference path="http/CplHttpRequester.ts"/> 
+///<reference path="html/CplHeloiseParser.ts"/> 
+module Crawler{
+    export class RagCrawler extends HeloiseCrawler{
+        
+    } 
+}
